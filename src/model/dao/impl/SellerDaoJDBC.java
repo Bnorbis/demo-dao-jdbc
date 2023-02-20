977e17e0aac939c0063e4947e92e492b6e9eece7
Bnorbis <brunanaian1@gmail.com>
2023-02-20 11:16:21 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,31 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    //Esqueleto para implementações
+    //Implementações de fato
+    public void insert (Seller obj){
+
+    }
+
+    public void update (Seller obj){
+
+    }
+
+    public void deleteById(Integer id){
+
+    }
+    public Seller findById(Integer id){
+        return null;
+    }
+
+    public List<Seller> findAll(){
+        return null;
+    }
+
+}
