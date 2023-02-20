dc520567107163451918193b1bdd7a92c9787d98
Bnorbis <brunanaian1@gmail.com>
2023-02-20 16:17:02 -0300

    FindById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,23 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    //Dependencia com atributo
+    private Connection conn;
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
 
     //Esqueleto para implementações
     //Implementações de fato
@@ -21,7 +33,37 @@ public class SellerDaoJDBC implements SellerDao {
 
     }
     public Seller findById(Integer id){
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName " +
+                            "FROM seller INNER JOIN department " +
+                            "ON seller.DepartmentId = department.Id " +
+                            "WHERE seller.Id = ?");
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            //Transformação em objetos associados Seller + dados do Departamento
+            if(rs.next()){
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                Seller obj = new Seller();
+                obj.setId(rs.getInt("Id"));
+                obj.setName(rs.getString("Name"));
+                obj.setBaseSalary(rs.getDouble("BaseSalary"));
+                obj.setBirthDate(rs.getDate("BirthDate"));
+                obj.setDepartment(dep);
+                return obj;
+            }
+            return null;
+        }catch (Exception e){
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
     }
 
     public List<Seller> findAll(){
